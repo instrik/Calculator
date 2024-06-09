56cc9c6b49de7043c4e326588508a7a4b2f6a06c
Aakash Agarwal <aakash@instrik.com>
2024-06-09 05:53:23 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'components/my_button.dart';
 import 'package:math_expressions/math_expressions.dart';
-// trial comment 4
+// trial comment 5
 void main() {
   runApp(
     MaterialApp(
