c5c856eaa3836bc4543503464ea1c31c69a9c8fc
Aakash Agarwal <aakash@instrik.com>
2024-07-29 01:55:42 +0530

    first

--- main.dart
+++ main.dart
@@ -6,6 +6,8 @@ import 'package:flutter/widgets.dart';
 import 'components/my_button.dart';
 import 'package:math_expressions/math_expressions.dart';
 
+//trial comment 1
+
 void main() {
   runApp(
     MaterialApp(
