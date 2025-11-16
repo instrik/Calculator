696829419f3fb366a6a86aca798f27ab06fa0262
Aakash Agarwal <agarwal7409@gmail.com>
2025-11-16 06:15:16 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -6,7 +6,7 @@ import 'package:flutter/widgets.dart';
 import 'components/my_button.dart';
 import 'package:math_expressions/math_expressions.dart';
 
-// trial comment 10
+// trial comment
 
 void main() {
   runApp(
@@ -283,6 +283,7 @@ class _CalculatorState extends State<Calculator> {
     );
   }
 }
+
 
 
 
