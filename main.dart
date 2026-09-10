04e627eb4489a96cec14ee4088e3093e7c83ec77
Aakash Agarwal <agarwal7409@gmail.com>
2026-09-11 00:56:33 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -7,7 +7,7 @@ import 'package:flutter/widgets.dart';
 import 'components/my_button.dart';
 import 'package:math_expressions/math_expresstlions.dart';
 
-// trial comment jkhjkhkkkjukhkljkjh
+// trial comment jkhjkhkkkjukhkljkjhkjh
 
 void main() {
   runApp(
